be4b2adf00ec5fd970649d2663edf793fe75b23c
andrewmagis <andrewmagis@gmail.com>
2014-10-28 15:53:09 -0700

    Adding vitaminD vs genetics vs supplementation analysis

---
+++ VitaminDGeneticsSupplementation.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# System imports
+import time, sys, argparse
+
+# PDF reports
+from pdf import GeneticsReport
+
+# Import the genetics processing library
+from gwasdb import GwasDB
+from actionabledb import ActionableDB
+from pharmacogenomicsdb import PharmacogenomicsDB
+
+# Import genome class
+from genome import Genome
+
+# Import DBSnp class
+from dbsnp import DBSnp
+from clinvar import Clinvar
+
+# Import the database class
+from database import Database
+
+from trait import Trait
+
+# Import the individual participant
+from participant import ParticipantDB
+
+def ArgParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--trait', required=True, default=None, help="Trait")
+    parser.add_argument('-m', '--measurement', required=True, default=None, help="Measurement")
+    args = parser.parse_args()
+    return args
+
+def main(parser):
+
+    # Set the output directory
+    output_dir = './results'
+
+    # Open connection to MySQL database
+    database = Database()
+
+    # Load the DBSnp database
+    dbsnp = DBSnp(database)
+
+    # Open connection to Clinvar database
+    clinvar_db = Clinvar(database)
+
+    # Create the participant DB from the database
+    participants = ParticipantDB(database, None, None, None, clinvar_db, dbsnp)
+
+    # Load the trait
+    #participants.LoadTrait('Fasting glucose-related traits')
+
+    print "Trait: %s"%(parser.trait)
+    print "Measurement: %s"%(parser.measurement)
+    participants.MetaboliteTraitCorrelation(parser.trait, parser.measurement)
+
+if __name__ == "__main__":
+    main(ArgParser())
+
