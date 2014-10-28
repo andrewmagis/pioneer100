ac3381c87f4f3f720fb374801c65692534a96e77
andrewmagis <andrewmagis@gmail.com>
2014-10-28 16:05:20 -0700

    Adding vitaminD vs genetics vs supplementation analysis

--- VitaminDGeneticsSupplementation.py
+++ VitaminDGeneticsSupplementation.py
@@ -55,7 +55,38 @@ def main(parser):
 
     # Loop over all the participants, and get the R1 and R2 metabolite + genetic score
     data = participants.MetaboliteTraitCorrelation('Vitamin D insufficiency', 'VITAMIN_D')
-    print data
+
+    # Generate histogram of the score column
+    (probability, bins) = np.histogram(x['Score'], bins=5)
+
+    print probability
+    print bins
+
+    # Now calculate the average
+    start = bins[0]
+    count = 1
+    for end in bins[1:]:
+
+        count += 1
+
+        # Get the subset of indices for these values
+        if (count == len(bins)):
+            indices = (x['Score'] >= start) & (x['Score'] <= end)
+        else:
+            indices = (x['Score'] >= start) & (x['Score'] < end)
+
+        # Get the values of round1 for these indices
+        subset = x['Round2'][indices]
+        subind = x['Score'][indices]
+
+        # Calculate the mean and stdev of these values
+        mean = np.nanmean(subset)
+        sd = np.nanstd(subset)
+        stderr = sd / math.sqrt(len(subset))
+
+        print mean, stderr
+
+        start = end
 
 
 if __name__ == "__main__":
