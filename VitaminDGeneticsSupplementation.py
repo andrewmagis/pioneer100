cc402685d55916b7ed295ec98182915e59b5f5d1
andrewmagis <andrewmagis@gmail.com>
2014-10-28 17:15:37 -0700

    Adding vitaminD vs genetics vs supplementation analysis

--- VitaminDGeneticsSupplementation.py
+++ VitaminDGeneticsSupplementation.py
@@ -22,8 +22,8 @@ from participant import ParticipantDB
 
 def ArgParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--trait', required=True, default="Vitamin D", help="Trait")
-    parser.add_argument('-m', '--measurement', required=True, default="VITAMIN_D", help="Measurement")
+    parser.add_argument('-t', '--trait', default="Vitamin D", help="Trait")
+    parser.add_argument('-m', '--measurement', default="VITAMIN_D", help="Measurement")
     args = parser.parse_args()
     return args
 
