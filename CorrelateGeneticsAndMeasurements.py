5bbc09d6ce69a6e14a8e05765476f3c80dd910bd
andrewmagis <andrewmagis@gmail.com>
2014-10-30 09:46:43 -0700

    Adding vitaminD vs genetics vs supplementation analysis

--- CorrelateGeneticsAndMeasurements.py
+++ CorrelateGeneticsAndMeasurements.py
@@ -82,20 +82,8 @@ def main(parser):
     genome = Genome()
     #genome.Load()
 
-    # Load the actionable database
-    actionable_db = ActionableDB(dbsnp, genome)
-    #actionable_db.Load(parser.actionable)
-
-    # Now load the GWAS database
-    gwas_db = GwasDB(dbsnp, genome)
-    #gwas_db.Load(parser.gwas)
-
-    # Load the pharmacogenomics database
-    pharm_db = PharmacogenomicsDB(dbsnp, genome)
-    #pharm_db.Load()
-
     # Create the participant DB from the database
-    participants = ParticipantDB(database, actionable_db, gwas_db, pharm_db, clinvar_db, dbsnp)
+    participants = ParticipantDB(database, None, None, None, clinvar_db, dbsnp)
 
     # Load the trait
     #participants.LoadTrait('Fasting glucose-related traits')
