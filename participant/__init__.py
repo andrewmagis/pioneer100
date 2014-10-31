b0fd47d67493977760357794246fb5fb9a7fb032
andrewmagis <andrewmagis@gmail.com>
2014-10-31 16:36:52 -0700

    DNAlysis variants

--- participant/__init__.py
+++ participant/__init__.py
@@ -795,7 +795,7 @@ class ParticipantDB(object):
 
                 f.write("%s"%(rsid))
 
-                for key in sorted(self.participants[key].traits['DNAlysis'].variants.keys()):
+                for key in sorted(self.participants.keys()):
                     f.write("\t%s"(self.participants[key].traits['DNAlysis'].variants[rsid].genotype))
                 f.write('\n')
 
