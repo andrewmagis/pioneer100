4979fded286b44923f5fb0c655a974bbfdc722d5
andrewmagis <andrewmagis@gmail.com>
2014-11-05 16:28:30 -0800

    Analyzing Alzheimers

--- participant/participant.py
+++ participant/participant.py
@@ -71,9 +71,8 @@ class Participant(object):
         # Get the measurement
         (dates, values, range) = self.GetMeasurement(measurement)
 
-        """
         # Check for CHOLESTEROL, DIABETES, or HYPERTENSION
-        if ("CHOLESTEROL" in measurement):
+        if ("CHOLESTEROL" in measurement) or ("TRIGLYCERIDES" in measurement):
             (med_dates, med_values, med_range) = self.GetMeasurement("MEDICATION_CHOLESTEROL")
             print med_dates, med_values
             if (med_values.size == 1) and (med_values == 1):
@@ -89,7 +88,6 @@ class Participant(object):
                 return None
             if (med_values.size == 2) and (med_values[1]==1):
                 return None
-        """
 
         if (values.size == 0):
             return None
