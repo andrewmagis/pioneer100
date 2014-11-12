8571792b782c5b79d1a2cb845d2988ed608e0094
andrewmagis <andrewmagis@gmail.com>
2014-11-12 14:27:51 -0800

    PG report final

--- LoadParticipantData.py
+++ LoadParticipantData.py
@@ -21,16 +21,23 @@ def main(parser):
     #database.CreateDataTable()
 
     # Load the data <- allocate some time to do this! It takes a while!
-    genova_files = ["./db/GenovaSpreadsheets/Genova.3.09232014/Hood.Nutreval.Binary.9.15.14 C.txt",
-             "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.Metsyn.Binary.9.15.14 C.txt",
-             "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.VitaminD.Binary.9.15.14 C.txt",
-             "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.NutrientToxicElements.Binary.9.15.14 C.txt"]
+    #genova_files = ["./db/GenovaSpreadsheets/Genova.3.09232014/Hood.Nutreval.Binary.9.15.14 C.txt",
+    #         "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.Metsyn.Binary.9.15.14 C.txt",
+    #         "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.VitaminD.Binary.9.15.14 C.txt",
+    #         "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.NutrientToxicElements.Binary.9.15.14 C.txt"]
+
+    genova_files = ["./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrEval.Binary.11.10.14.txt",
+             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.MetSyn.Binary.11.10.14.txt",
+             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.VitaminD.Binary.11.10.14.txt",
+             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrientToxicElements.Binary.11.10.14.txt"]
 
     for f in genova_files:
         print "Loading file", f
         database.LoadGenova(f)
 
-    quest_files = ["./db/QuestSpreadsheets/Quest.3.09232014/all.quest.csv"]
+    #quest_files = ["./db/QuestSpreadsheets/Quest.3.09232014/all.quest.csv"]
+    quest_files = ["./db/QuestSpreadsheets/Quest.4.11122014/all.quest.csv"]
+    quest_files = ["./db/QuestSpreadsheets/Quest.4.11122014/all.quest.csv"]
 
     for f in quest_files:
         print "Loading file", f
