0e5e3838173531d1a8cb956c6ecef183a36cef4f
andrewmagis <andrewmagis@gmail.com>
2014-11-12 17:00:54 -0800

    QS analysis with variant

--- AnalyzeQSData.py
+++ AnalyzeQSData.py
@@ -8,6 +8,10 @@ from database import Database
 from participant import ParticipantDB
 from qs import QS
 
+# Import DBSnp class
+from dbsnp import DBSnp
+from clinvar import Clinvar
+
 def ArgParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--fitbit', default=None, action='store_true', help="Update Fitbit data")
