e6172cb2124c6aae67a52c5e46e5de47242fbccb
andrewmagis <andrewmagis@gmail.com>
2014-11-15 14:52:04 -0800

    QS Measurement

--- AnalyzeQSData.py
+++ AnalyzeQSData.py
@@ -35,7 +35,7 @@ def main(parser):
     # Load the participants
     participants = ParticipantDB(database, None, None, None, clinvar_db, dbsnp)
 
-    qs.AnalyzeQSMeasurement(participants, 'Exercise Responsiveness', 'GLUCOSE')
+    qs.AnalyzeQSMeasurement(participants, 'Exercise Responsiveness', 'INSULIN')
 
 
 if __name__ == "__main__":
