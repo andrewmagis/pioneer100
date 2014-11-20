98bacb6f445681f5cbc47fc548b0f8d218ecb625
andrewmagis <andrewmagis@gmail.com>
2014-11-20 10:30:18 -0800

    proteomics data

--- proteomics/__init__.py
+++ proteomics/__init__.py
@@ -207,7 +207,7 @@ class Proteomics(object):
         # Query the table for the keys
         protein_ids = []
         for protein in header:
-            result = cursor.execute("SELECT protein_id FROM prot_proteins WHERE (abbreviation = 'IL_8')")
+            result = cursor.execute("SELECT protein_id FROM prot_proteins WHERE abbreviation = (%s)", (protein,))
             print result
             protein_ids.append()
 
