67fd6db1afa492909f99082c1490a46d52a0a2b3
andrewmagis <andrewmagis@gmail.com>
2014-11-21 16:42:33 -0800

    proteomics data

--- proteomics/__init__.py
+++ proteomics/__init__.py
@@ -11,16 +11,17 @@ class Proteomics(object):
     def __init__(self, database):
         self.database = database
 
-    def Get(self, username, round):
+    def _get_val(self, username, round):
 
         cursor = self.database.GetCursor()
-        cursor.execute("SELECT v.ct_value FROM prot_observations as o, prot_values as v "
+        cursor.execute("SELECT v.norm_value FROM prot_observations as o, prot_values as v "
                        "WHERE o.username = (%s) AND o.round = (%s) AND v.observation_id = o.observation_id "
                        "ORDER BY o.observation_id", (username,round,))
 
         # Build numpy array out of result
-        return np.array(list(cursor.fetchall()), dtype=[('ct_value', float)])
+        return np.array(list(cursor.fetchall()), dtype=[('norm_value', float)])
 
+    # Ignore this function
     def GetNormalized(self, username, round):
 
         # Get the protein control ids as well as the ct values
@@ -44,72 +45,6 @@ class Proteomics(object):
 
         # Calculate the mean negative control value
         mean_negative_controls = np.mean(controls['negative_control'], axis=1)
-
-        # Normalize the
-
-
-    def Compile(self):
-
-        username = '2682430'
-        round = 1
-
-        # Make a really big matrix of round 1 and round 2 for all participants
-        result = self.GetData(username)
-
-        columns = [d[0] for d in cursor.description]
-        r1 = np.array([x for x in cursor])
-        print r1
-
-        for key in sorted(self.participants.keys()):
-
-            result = self.participants[key].MetaboliteTraitCorrelation(trait, measurement, pvalue)
-
-            if (not result is None):
-                data.append(result)
-
-            print "Processed %s"%(key)
-
-        # Build numpy structured array of scores
-        x = np.array(data, dtype=[('Username', np.str, 10), ('Gender', np.str, 1), ('Round1', float), ('Round2', float), ('Round3', float), ('Score', float)])
-
-
-    def GetData(self, username):
-
-        # Get the cursor
-        cursor = self.database.GetCursor();
-
-        cursor.execute("SELECT * FROM metabolomics WHERE username = (%s) ORDER BY ROUND", (username,))
-        result = None
-        columns = [d[0] for d in cursor.description]
-
-        # Concatenate all the tuples together
-        result = []
-        for row in cursor:
-            result.append(row)
-
-        if (len(result)>0):
-            result = zip(*result)
-
-        if (result is None):
-            return {}
-
-        if (len(result)==0):
-            return {}
-
-        # Now convert each value into a numpy array
-        final = {}
-        for key, values in zip(columns, result):
-            if (key == "USERNAME"):
-                continue;
-            if (key == "ROUND"):
-                final[key] = np.array(values, dtype=np.object)
-                continue;
-
-            try:
-                final[key] = np.array(values, dtype=np.float)
-            except:
-                final[key] = np.array(values, dtype=np.object)
-        return final
 
     def Clean(self, value):
 
