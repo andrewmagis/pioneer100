4ee730b0c189be024ed9e6e6fc98281b338336d7
andrewmagis <andrewmagis@gmail.com>
2014-11-24 14:48:48 -0800

    clinical chemistries

--- LoadParticipantData.py
+++ LoadParticipantData.py
@@ -19,7 +19,7 @@ def main(parser):
     database = Database()
 
     # If necessary, build the data table
-    #database.CreateDataTable()
+    database.CreateDataTable()
 
     # Load the data <- allocate some time to do this! It takes a while!
     #genova_files = ["./db/GenovaSpreadsheets/Genova.3.09232014/Hood.Nutreval.Binary.9.15.14 C.txt",
@@ -27,14 +27,23 @@ def main(parser):
     #         "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.VitaminD.Binary.9.15.14 C.txt",
     #         "./db/GenovaSpreadsheets/Genova.3.09232014/Hood.NutrientToxicElements.Binary.9.15.14 C.txt"]
 
-    genova_files = ["./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrEval.Binary.11.10.14.txt",
-             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.MetSyn.Binary.11.10.14.txt",
-             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.VitaminD.Binary.11.10.14.txt",
-             "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrientToxicElements.Binary.11.10.14.txt"]
+
+    #genova_files = ["./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrEval.Binary.11.10.14.txt",
+    #         "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.MetSyn.Binary.11.10.14.txt",
+    #         "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.VitaminD.Binary.11.10.14.txt",
+    #         "./db/GenovaSpreadsheets/Genova.4.11122014/Hood.NutrientToxicElements.Binary.11.10.14.txt"]
+
+
+    genova_files = ["./db/GenovaSpreadsheets/Genova.5.11212014/Hood.NutrEval.Binary.11.21.14.txt",
+             "./db/GenovaSpreadsheets/Genova.5.11212014/Hood.MetSyn.Binary.11.21.14.txt",
+             "./db/GenovaSpreadsheets/Genova.5.11212014/Hood.VitD.Binary.11.21.14.txt",
+             "./db/GenovaSpreadsheets/Genova.5.11212014/Hood.NutrientToxicElements.Binary.11.21.14.txt"]
 
     for f in genova_files:
         print "Loading file", f
         database.LoadGenova(f)
+
+    return
 
     #quest_files = ["./db/QuestSpreadsheets/Quest.3.09232014/all.quest.csv"]
     quest_files = ["./db/QuestSpreadsheets/Quest.4.11122014/all.quest.csv"]
