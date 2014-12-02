32451758e7c2b9fdad3e3bc64ac5318533747493
andrewmagis <andrewmagis@gmail.com>
2014-12-02 12:35:34 -0800

    clinical chemistries

--- chemistries/__init__.py
+++ chemistries/__init__.py
@@ -266,9 +266,6 @@ class Chemistries(object):
                             # Just insert the row
                             cursor.execute("INSERT INTO chem_observations (username, round, date) VALUES (%s,%s, %s)", (username, round, date_ordered))
 
-                            self.database.Commit()
-                            cursor = self.database.GetCursor()
-
                             # Get the last observation id and create the data tuple
                             data.append((cursor.lastrowid, mapping[id], self.Clean(current[id])))
 
