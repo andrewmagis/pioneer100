90bffd50615700e55f0226c39e8b26bed1795084
andrewmagis <andrewmagis@gmail.com>
2014-12-02 12:00:46 -0800

    clinical chemistries

--- chemistries/__init__.py
+++ chemistries/__init__.py
@@ -268,7 +268,7 @@ class Chemistries(object):
                             cursor.execute("INSERT INTO chem_observations (username, round, date) VALUES (%s,%s, %s)", (username, round, date_ordered))
 
                             # Get the last observation id and create the data tuple
-                            data.append((cursor.lastrowid, mapping[id], self.Clean(current[id])))
+                            data.append((cursor.lastrowid, mapping[id], self.Clean(current[id], username, round, date_ordered)))
 
         # Insert the observations
         result = cursor.executemany("INSERT INTO chem_values (observation_id, chemistry_id, value) VALUES (%s,%s, %s)", data)
