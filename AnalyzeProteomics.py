06fe7a6ce7f99605849775e3b4f0f9eb5368b4c5
andrewmagis <andrewmagis@gmail.com>
2014-12-02 17:20:08 -0800

    clinical chemistries

--- AnalyzeProteomics.py
+++ AnalyzeProteomics.py
@@ -53,7 +53,7 @@ def main(parser):
     """
 
     print "Getting proteomics data"
-    result = prots._get_diff('1631868', 1, 2)
+    result = prots._get_diff('1216743', 1, 2)
     print result
 
     print "Getting qs data"
@@ -62,7 +62,7 @@ def main(parser):
     SECOND_BLOOD_DRAW=datetime(2014, 8, 1)
     THIRD_BLOOD_DRAW=datetime(2014, 11, 1)
 
-    result = qs.get_avg_val('1631868', FIRST_BLOOD_DRAW, SECOND_BLOOD_DRAW)
+    result = qs.get_avg_val('1216743', FIRST_BLOOD_DRAW, SECOND_BLOOD_DRAW)
     print result
 
 
